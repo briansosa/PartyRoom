38d5980ed9cab40752c85e17a980b7ae50ae1f4d
Brian Nicolas Sosa <briana@lagash.com>
2020-03-02 17:31:13 -0300

    Add test method 'EventService_GetAllEventForRepository'

--- Tests/BusinessTest/EventServiceTest.cs
+++ Tests/BusinessTest/EventServiceTest.cs
@@ -6,6 +6,7 @@ using DTO;
 using System;
 using Domain;
 using DatabaseInterface;
+using System.Collections.Generic;
 
 namespace BusinessTest
 {
@@ -14,25 +15,47 @@ namespace BusinessTest
         public Mock<IEventRepository> IEventRepositoryMock = new Mock<IEventRepository>();
 
         [Fact]
-        public void EventService_Add_1(){
+        public void EventService_Add_Return_1(){
             // Arrange
             DtoEvent dto = new DtoEvent(){
                 Name = "Pepe",
                 DateStart = DateTime.Now,
                 DateFinish = DateTime.Now.AddDays(1)  
             };
-            // Event eventData = new Event(){
-            //     Name = "Pepe",
-            //     DateStart = DateTime.Now,
-            //     DateFinish = DateTime.Now.AddDays(1) 
-            // };
             IEventRepositoryMock.Setup(p => p.Add(It.IsAny<Event>())).Returns(1);
             EventService eventServiceTest = new EventService(IEventRepositoryMock.Object);
 
             // Act
             int id = eventServiceTest.Add(dto);
             // Assert
-            id.Should().Equals(12);
+            id.Should().Be(1);
+        }
+
+        [Fact]
+        public void EventService_GetAllEventForRepository()
+        {
+            // Arrange
+            DateTime start = DateTime.Now;
+            DateTime finish = DateTime.Now.AddDays(1);
+            List<Event> listEvents = new List<Event>();
+            listEvents.Add(new Event(){
+                Name = "Pepe",
+                DateStart = start,
+                DateFinish = finish
+            });
+            List<DtoEventResponse> listResponse = new List<DtoEventResponse>();
+            listResponse.Add(new DtoEventResponse 
+                {
+                    Name = "Pepe",
+                    DateStart = start,
+                    DateFinish = finish
+                });
+            IEventRepositoryMock.Setup(p => p.Get()).Returns(listEvents);
+            EventService eventService = new EventService(IEventRepositoryMock.Object);
+            // Act
+            List<DtoEventResponse> listDtos = eventService.Get();
+            // Assert
+            listDtos.Should().BeEquivalentTo(listResponse);
         }
     }
 }
